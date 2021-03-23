ffa6dc5a654b953122475ac7452ab6c5e8c800f8
alekseyIsakin <karahunforever@gmail.com>
2021-03-23 22:13:25 +0800

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testApp
+{
+    ref struct TestClass<T>
+    {
+        Span<T> Tspan;
+    }
+    class Class1<T>
+    {
+        TestClass<string> test;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1<string> t = new Class1<string>();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
